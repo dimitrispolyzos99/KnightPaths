063a35cb1eb115e114a005efc270aafc30f824e9
Dimitrispol99 <dimitrispoluzos99@hotmail.com>
2026-06-09 18:41:08 +0300

    Initial Commit

---
+++ KnightPaths/KnightPathsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnightPathsApp.swift
+//  KnightPaths
+//
+//  Created by Dimitris Poluzos on 9/6/26.
+//
+
+import SwiftUI
+
+@main
+struct KnightPathsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
